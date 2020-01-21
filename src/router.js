4b1d5aa6196e5f6b66513188d00cd204f6cd8020
Samantha <samanthaf@deardorffinc.com>
2020-01-21 13:59:26 -0500

    add in express

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from './views/Home.vue';
-import Resume from './views/Resume.vue';
-import Contact from './views/Contact.vue';
-import Portfolio from './views/Portfolio.vue';
-import ProjectOne from './views/ProjectOne.vue';
-import ProjectTwo from './views/ProjectTwo.vue';
-import ProjectThree from './views/ProjectThree.vue';
-
+// import Home from './views/Home.vue';
+// import Resume from './views/Resume.vue';
+// import Contact from './views/Contact.vue';
+// import Portfolio from './views/Portfolio.vue';
+// import ProjectOne from './views/ProjectOne.vue';
+// import ProjectTwo from './views/ProjectTwo.vue';
+// import ProjectThree from './views/ProjectThree.vue';
+import Posts from '../src/views/Posts'
 
 
 Vue.use(VueRouter);
@@ -16,63 +16,71 @@ export default new VueRouter({
     mode: 'history',
     routes: [{
 
-            path: '',
-            name: 'home',
+            path: '/posts',
+            name: 'posts',
             // component: () =>
             //     import ('./views/Products.vue') or
-            component: Home,
-        }, {
-            path: '/home',
-            name: 'home',
-            // component: () =>
-            //     import ('./views/Products.vue') or
-            component: Home,
-        }, {
-            path: '/resume',
-            name: 'resume',
-            // component: () =>
-            //     import ('./views/Products.vue') or
-            component: Resume,
-        }, {
-            path: '/contact',
-            name: 'contact',
-            // component: () =>
-            //     import ('./views/Products.vue') or
-            component: Contact,
-        }, {
-            path: '/portfolio',
-            name: 'portfolio',
-            // component: () =>
-            //     import ('./views/Products.vue') or
-            component: Portfolio,
-        }, {
-            path: '/portfolio',
-            name: 'portfolio',
-            // component: () =>
-            //     import ('./views/Products.vue') or
-            component: Portfolio,
-            children: [{
-                    path: 'projectone',
-                    name: 'projectone',
-                    // component: () =>
-                    //     import ('./views/Products.vue') or
-                    component: ProjectOne,
-                }, {
-                    path: 'projecttwo',
-                    name: 'projecttwo',
-                    // component: () =>
-                    //     import ('./views/Products.vue') or
-                    component: ProjectTwo,
-                }, {
-                    path: 'projectthree',
-                    name: 'projectthree',
-                    // component: () =>
-                    //     import ('./views/Products.vue') or
-                    component: ProjectThree,
-                },
-
-            ]
+            component: Posts,
         },
+        // {
+
+        //     path: '',
+        //     name: 'home',
+        //     // component: () =>
+        //     //     import ('./views/Products.vue') or
+        //     component: Home,
+        // }, {
+        //     path: '/home',
+        //     name: 'home',
+        //     // component: () =>
+        //     //     import ('./views/Products.vue') or
+        //     component: Home,
+        // }, {
+        //     path: '/resume',
+        //     name: 'resume',
+        //     // component: () =>
+        //     //     import ('./views/Products.vue') or
+        //     component: Resume,
+        // }, {
+        //     path: '/contact',
+        //     name: 'contact',
+        //     // component: () =>
+        //     //     import ('./views/Products.vue') or
+        //     component: Contact,
+        // }, {
+        //     path: '/portfolio',
+        //     name: 'portfolio',
+        //     // component: () =>
+        //     //     import ('./views/Products.vue') or
+        //     component: Portfolio,
+        // }, {
+        //     path: '/portfolio',
+        //     name: 'portfolio',
+        //     // component: () =>
+        //     //     import ('./views/Products.vue') or
+        //     component: Portfolio,
+        //     children: [{
+        //             path: 'projectone',
+        //             name: 'projectone',
+        //             // component: () =>
+        //             //     import ('./views/Products.vue') or
+        //             component: ProjectOne,
+        //         }, {
+        //             path: 'projecttwo',
+        //             name: 'projecttwo',
+        //             // component: () =>
+        //             //     import ('./views/Products.vue') or
+        //             component: ProjectTwo,
+        //         }, {
+        //             path: 'projectthree',
+        //             name: 'projectthree',
+        //             // component: () =>
+        //             //     import ('./views/Products.vue') or
+        //             component: ProjectThree,
+        //         },
+
+        //     ]
+        // },
 
     ]
 
